427b57f79019712306b7b27764291fb0c4c096c4
freejacque <freejacque@gmail.com>
2015-11-15 19:10:29 -0500

    created mongo connection

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict';
 
 var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(
+  'mongodb://127.0.0.1:27017/accounting',
+  function(err, connection) {
+    var collection = connection.connection('customers');
+
+    collection.insert({'name': 'Jane Doe'}, function(err, count) {
+
+      collection.find().toArray(function(err, documents) {
+        console.dir(documents);
+        connection.close();
+      });
+
+    });
+
+  });
