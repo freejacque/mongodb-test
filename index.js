6e48bc004e1f909cc9503b4d9b029dc1f2f42a39
freejacque <freejacque@gmail.com>
2015-11-23 20:56:55 -0500

    created update function

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ MongoClient.connect(
     // collection within the accounting db
     var collection = connection.connection('customers');
 
+    collection.update({}, {'$set': {'age': 24}}, function(err, count) {
+
+      console.log('Updated', count, 'documents');
+
+      collection.find().toArray(function(err, documents) {
+        console.dir(documents);
+        connection.close();
+      });
+
+    });
     // use the insert method from the customers collection
     collection.insert({'name': 'Jane Doe'}, function(err, count) {
       collection.find().toArray(function(err, documents) {
